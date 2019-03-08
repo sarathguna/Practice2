7be902df01d56e2de74fd9e65ed84b06f911ce36
sarath <sarathgunalan@gmail.com>
2019-03-08 12:42:31 +0530

    New File Modified

--- sarath.java
+++ sarath.java
@@ -1,1 +1,17 @@
-a=10
+public class sarath
+
+{
+  public static void main(String[] args) {
+        int rows = 5;
+
+        for(int i = 1; i <= rows; ++i) {
+ 
+     		for(int j = 1; j <= i; ++j) {
+     
+	     		System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
